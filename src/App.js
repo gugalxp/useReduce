27b2a6aadb6753a1b25f3902e93ae73aa91cd191
Gustavo Arruda <gustavoleone3456@hotmail.com>
2023-06-12 21:39:00 -0300

    exemplo do hook useReduce

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer } from 'react';
+
+const initialState = { loading: true, data: null, error: null };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'fetchData':
+      return { ...state, loading: true };
+    case 'fetchDataSuccess':
+      return { ...state, loading: false, data: action.payload, error: null };
+    case 'fetchDataError':
+      return { ...state, loading: false, data: null, error: action.payload };
+    default:
+      throw new Error();
+  }
+}
+
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    // Lógica assíncrona simulada (por exemplo, uma chamada a uma API)
+    setTimeout(() => {
+      const randomData = Math.random();
+      if (randomData > 0.5) {
+        resolve(randomData);
+      } else {
+        reject(new Error('Erro ao buscar os dados.'));
+      }
+    }, 1000);
+  });
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: 'fetchData' });
+
+    fetchData()
+      .then((data) => {
+        console.log(data);
+        dispatch({ type: 'fetchDataSuccess', payload: data });
+      })
+      .catch((error) => {
+        dispatch({ type: 'fetchDataError', payload: error });
+      });
+  }, []);
+
+  if (state.loading) {
+    return <div>Carregando...</div>;
+  }
+
+  if (state.error) {
+    return <div>Erro: {state.error.message}</div>;
+  }
+
+  return <div>Dados: {state.data}</div>;
 }
 
 export default App;
